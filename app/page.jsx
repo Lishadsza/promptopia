3e2d2fd7afc4f2fb6fc5940b77e6db7eb0c0dd4b
Lishadsza <22b18.lisha@sjec.ac.in>
2024-09-05 18:54:59 +0530

    home page design

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+
+
+const homepage = () => {
+  return (
+     <section className="w-full flex-center flex-col">
+      <h1 className="head_text ">
+        Discover & Share
+      </h1>
+     </section>
+  )
+}
+
+export default homepage
