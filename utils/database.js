32dbd3e3b6ae8786e726a38bb9a827d7896ba9cb
Lishadsza <22b18.lisha@sjec.ac.in>
2024-09-12 00:55:24 +0530

    router

--- utils/database.js
+++ utils/database.js
@@ -3,9 +3,23 @@ import mongoose from 'mongoose';
 let isConnected=false;
 
 export const connecttoDB=async() =>{
-    mongoose.set('strictQuery',true);
+    mongoose.set('strictQuery', true);
 }
 
 if(isConected){
-    console.log('MongoDB is already')
+    console.log('MongoDB is already connected')
+    return;
+}
+
+try{
+    await mongoose.connect(process.env.MONGODB_URI, {
+    dbName:"share_prompt",
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    })
+
+    isConnected=true;
+    console.log('MongoDB connected')
+} catch(error){
+
 }
