d6ab09a3254edaee94e02bb86f2ac65dcc39cf46
Lishadsza <22b18.lisha@sjec.ac.in>
2024-09-23 21:14:50 +0530

    profile page and new route

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -32,6 +32,7 @@ const Feed = () => {
       const data=await response.json();
       setPosts(data);
     }
+    console.log(posts);
     fetchPosts();
   },[]);
 
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,7 +7,12 @@ import {usePathname,useRouter} from 'next/navigation';
 
 const PromptCard = ({ post,handleTagClick,handleEdit,handleDelete}) => {
     const [copied,setCopied]=useState("");
-  
+    const handleCopy=()=>{
+      setCopied(post.prompt);
+      navigator.clipboard.writeText(post.prompt);
+      setTimeout(()=> setCopied(""),3000);
+    }
+    
 
 
 
@@ -29,15 +34,15 @@ const PromptCard = ({ post,handleTagClick,handleEdit,handleDelete}) => {
             <p className="font-inter text-sm text-gray-500"> {post.creator?.email}</p> 
           </div>
         </div>
-        <div className="copy_btn" onClick={()=>{}}>
+        <div className="copy_btn" onClick={handleCopy}>
           <Image 
           src={copied===post.prompt
           ? '/assets/icons/tick.svg'
           : '/assets/icons/copy.svg'
          }
          width={12}
-         height={12}
-         />
+         height={12}/>
+         
         </div>
       </div>
 
