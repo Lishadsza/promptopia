3e140d829019eee80cb454f5c45802287ac3e218
paigauresh <paigauresh@gmail.com>
2024-10-04 16:45:36 +0530

    build issues fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["mongoose"],
-  },
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-  },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    }
-    return config
-  }
+  /* config options here */
 }
 
-export default  nextConfig;
-
+module.exports = nextConfig
