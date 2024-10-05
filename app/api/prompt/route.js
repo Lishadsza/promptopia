919623eb74011af18ad3af34d2c577496a17374e
Lishadsza <22b18.lisha@sjec.ac.in>
2024-10-05 15:33:13 +0530

    bug fixture @gaureshpai

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,18 +1,15 @@
-import {connectToDB} from '@utils/database';
-import Prompt from '@models/prompt';
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+import { revalidatePath } from "next/cache";
 
+export const GET = async (request) => {
+    try {
+        await connectToDB()
 
-export const GET =async(request)=>{
-    try{
-        await connectToDB();
-
-        const prompts=await Prompt.find({}).populate('creator');
-
-        return new Response(JSON.stringify(prompts),{
-        status:200})
-        
-      }catch(error){
-       return new Response("Failed to fetch all prompts",{
-       status:200})
-}
-}
+        const prompts = await Prompt.find({}).populate('creator')
+        revalidatePath("/")
+        return new Response(JSON.stringify(prompts), { status: 200 })
+    } catch (error) {
+        return new Response("Failed to fetch all prompts", { status: 500 })
+    }
+} 
